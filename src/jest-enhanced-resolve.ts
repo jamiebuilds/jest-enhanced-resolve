e516a0fc44131cb85b56f31978dc50102f3f7382
v1rtl <pilll.pl22@gmail.com>
2021-05-30 13:08:10 +0300

    update enhanced-resolve to latest

--- src/jest-enhanced-resolve.ts
+++ src/jest-enhanced-resolve.ts
@@ -1,36 +1,37 @@
-import fs from 'fs';
-import path from 'path';
+import * as fs from 'fs'
+import path from 'path'
 // import resolveFrom from 'resolve-from';
-import { ResolverFactory } from 'enhanced-resolve';
-import defaultResolve from "jest-resolve/build/defaultResolver";
+import { ResolverFactory } from 'enhanced-resolve'
+import defaultResolve from 'jest-resolve/build/defaultResolver'
 
 interface JestResolveOpts {
-  basedir: string,
-  browser: boolean,
-  extensions: Array<string>,
-  moduleDirectory: Array<string>,
-  paths: Array<string>,
-  rootDir: Array<string>,
+  basedir: string
+  browser: boolean
+  extensions: Array<string>
+  moduleDirectory: Array<string>
+  paths: Array<string>
+  rootDir: Array<string>
 }
 
-export default function createEnhancedResolver(resolverOpts: ResolverFactory.ResolverOption) {
+export default function createEnhancedResolver(resolverOpts: Parameters<typeof ResolverFactory.createResolver>[0]) {
   return function enhancedResolve(modulePath: string, opts: JestResolveOpts) {
     if (modulePath.startsWith('.') || modulePath.startsWith(path.sep)) {
-      return defaultResolve(modulePath, opts);
+      return defaultResolve(modulePath, opts)
     }
 
     let wpResolver = ResolverFactory.createResolver({
-      fileSystem: fs,
       ...resolverOpts,
-      useSyncFileSystemCalls: true,
-    });
+      // @ts-ignore
+      fileSystem: fs,
+      useSyncFileSystemCalls: true
+    })
 
-    let result = wpResolver.resolveSync({}, opts.basedir, modulePath);
+    let result = wpResolver.resolveSync({}, opts.basedir, modulePath)
 
     if (result) {
-      result = fs.realpathSync(result);
+      result = fs.realpathSync(result)
     }
 
-    return result;
-  };
-};
+    return result
+  }
+}
