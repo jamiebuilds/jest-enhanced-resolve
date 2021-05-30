db21ff099ac2a72ddeb89a2480e9eb7fcd342927
v1rtl <pilll.pl22@gmail.com>
2021-05-30 18:19:55 +0300

    fix export error (and use new typings for jest config)

--- src/jest-enhanced-resolve.ts
+++ src/jest-enhanced-resolve.ts
@@ -2,21 +2,16 @@ import * as fs from 'fs'
 import path from 'path'
 // import resolveFrom from 'resolve-from';
 import { ResolverFactory } from 'enhanced-resolve'
-import defaultResolve from 'jest-resolve/build/defaultResolver'
-
-interface JestResolveOpts {
+import Resolver from 'jest-resolve'
+import type { ResolverConfig } from 'jest-resolve/build/types'
+interface JestResolveOpts extends ResolverConfig {
   basedir: string
-  browser: boolean
-  extensions: Array<string>
-  moduleDirectory: Array<string>
-  paths: Array<string>
-  rootDir: Array<string>
 }
 
 export default function createEnhancedResolver(resolverOpts: Parameters<typeof ResolverFactory.createResolver>[0]) {
   return function enhancedResolve(modulePath: string, opts: JestResolveOpts) {
     if (modulePath.startsWith('.') || modulePath.startsWith(path.sep)) {
-      return defaultResolve(modulePath, opts)
+      return new Resolver(modulePath, opts)
     }
 
     let wpResolver = ResolverFactory.createResolver({
